a0121a2a288aa59e5de585584cb4c91bd2519388
unknown <krishna2121.rana@gmail.com>
2018-06-24 14:34:12 +0200

    store data in localstorage

--- javascript/module-2/week7-class/js/main.js
+++ javascript/module-2/week7-class/js/main.js
@@ -1,13 +1,8 @@
 var carName= document.querySelector(".car-name");
 var carYear= document.querySelector(".car-year");
 var carAdd= document.querySelector(".add-car");
-let carList= [];
-
-
-
-carAdd.addEventListener("click", addCar);
-
-
+var carSave= document.querySelector(".save-car");
+let carList= JSON.parse(window.localStorage.carList);;
 function addCar(){
 	if(carName.value ===""){
 		alert("Please Enter Name ")
@@ -22,14 +17,20 @@ function addCar(){
 	}
 	
 }
-
-
 function showCar(){
 	let carOutput= ``;
+	if(carList.length===0){
+		carOutput=`<p>There is no Data to show</p>`
+	}
 	for(let i=0; i<carList.length; i++){
-		carOutput += `<li>Car Name: ${carList[i].name}, Year: ${carList[i].year}</li>`
+		carOutput += `<li><strong>Car Name:</strong> ${carList[i].name}, <strong>Year:</strong> ${carList[i].year}</li>`
 	}
 	document.querySelector(".car-list").innerHTML = carOutput;
+	localStorage.setItem("carList", JSON.stringify(carList));
 }
 
+
+localStorage.setItem("carList", JSON.stringify(carList));
 showCar();
+carAdd.addEventListener("click", addCar);
+
