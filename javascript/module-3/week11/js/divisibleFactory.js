cffac83ff867f03430c99b10ff61b6476f6c861a
unknown <krishna2121.rana@gmail.com>
2018-08-07 18:50:58 +0200

    Week 11 homework closures

---
+++ javascript/module-3/week11/js/divisibleFactory.js
@@ -0,0 +1,44 @@
+let arr = [];
+for (let i = 1; i <= 1000; i++) {
+    arr.push(i);
+}
+console.log(arr);
+
+// here please start your solution
+// using closures, functions and (map,filter,reduce)
+const divisibleFactory = function(z) {
+    return function() {
+        return arr.filter((number) => {
+            return number % z === 0;
+        });
+    }
+}
+const divisbleBy3 = divisibleFactory(3);
+const divisbleBy10 = divisibleFactory(10);
+const divisbleBy21 = divisibleFactory(21);
+console.log(divisbleBy3());
+console.log(divisbleBy10());
+console.log(divisbleBy21());
+
+let totalDivisibleNumbers = [];
+
+for (let i = 1; i <= 30; i++) {
+    totalDivisibleNumbers.push(divisibleFactory(i)().length);
+}
+
+console.log(totalDivisibleNumbers);
+
+
+// html output
+document.getElementById("homework2_2").innerHTML = `${
+  divisbleBy3()
+}`
+document.getElementById("homework2_3").innerHTML = `${
+  divisbleBy10()
+}`
+document.getElementById("homework2_4").innerHTML = `${
+  divisbleBy21()
+}`
+document.getElementById("homework2_5").innerHTML = `${
+  totalDivisibleNumbers
+}`
